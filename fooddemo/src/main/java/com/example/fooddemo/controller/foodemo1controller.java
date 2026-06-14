ae303b985b45bb8c9c38da7215c7891698a2cf6b
pochamreddyanjali <pochamreddyanjali2005@gmail.com>
2026-06-14 21:40:01 +0530

    Learned about crud operations using jpa interface

--- fooddemo/src/main/java/com/example/fooddemo/controller/foodemo1controller.java
+++ fooddemo/src/main/java/com/example/fooddemo/controller/foodemo1controller.java
@@ -1,5 +1,9 @@
 package com.example.fooddemo.controller;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +19,38 @@ public class foodemo1controller {
 	{
 		return foodservice.addEmployee(emp);
 		
+	}
+	@GetMapping("/getinfo")
+	public List<Employee> getEmployees()
+	{
+		return foodservice.getEmployee();
+	}
+	@GetMapping("/getId/{id}")
+	public Object getEmployeeById(@PathVariable int id)
+	{
+		Employee emp = foodservice.findId(id);
+
+	    if(emp != null)
+	    {
+	        return emp;
+	    }
+
+	    return "Employee Id Not Found";
+	}
+	@GetMapping("/deleteId/{id}")
+	public String deleteById(@PathVariable int id)
+	{
+		Employee emp = foodservice.findId(id);
+		if(emp==null)
+		{
+			return "Employee id not found to delete";
+		}
+		foodservice.deleteId(id);
+		return "Employee id deleted Sucessfully";
+	}
+	@PostMapping("/update")
+	public String updateEmployee(@RequestBody Employee emp)
+	{
+		return foodservice.update(emp);
 	}
    }
--- fooddemo/src/main/java/com/example/fooddemo/service/FoodService.java
+++ fooddemo/src/main/java/com/example/fooddemo/service/FoodService.java
@@ -1,4 +1,6 @@
 package com.example.fooddemo.service;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.fooddemo.entity.Employee;
@@ -13,5 +15,26 @@ public class FoodService {
     	repo.save(user);
     	return "Employee Added SucessFully";
     }
-	
+    public List<Employee> getEmployee()
+    {
+    	return repo.findAll();
+    }
+    public Employee findId(int id)
+    {
+    	return repo.findById(id).orElse(null);
+    }
+    public void deleteId(int id)
+    {
+    	
+    	 repo.deleteById(id);
+    }
+    public String update(Employee emp)
+    {
+    	if(repo.existsById(emp.getId()))
+    	{
+    		repo.save(emp);
+    		return "Employee updated Suceesfully";
+    	}
+    	return "Employee not found to be updated";
+    }
 }
